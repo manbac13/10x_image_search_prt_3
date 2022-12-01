43b4bf7bbdea464b2b398928dadc9d3a500bfbe8
manbac13 <manishbachhav98@gmail.com>
2022-12-01 20:15:07 +0530

    files uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [formData, setFormData] = useState({})
+
+  const [imageData, setImageData] = useState([])
+  const [display, setDisplay] = useState("none")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const query = formData.query;
+
+  const handleClick = () => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
+      params: { q: query, pageNumber: '1', pageSize: '10', autoCorrect: 'true' },
+      headers: {
+        'X-RapidAPI-Key': 'e1de50583fmsh62d90f9b37942eap1e3e29jsnc5210b9eccc2',
+        'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      setImageData(response.data.value)
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+    console.log(imageData)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-div">
+
+        <div className="header-div">
+          <h1>React Photo Search</h1>
+          <button>Bookmarks</button>
+        </div>
+
+        <div className="search-div">
+          <form onClick={handleSubmit}>
+            <input type="text" placeholder="Search free high resolution images"
+              onChange={e => setFormData({ ...formData, query: e.target.value })} />
+            <button onClick={handleClick}>Search</button>
+          </form>
+        </div>
+
+
+      </div>
+      <div className='images-div'>
+        {
+          imageData.map((image, index) => {
+            return (
+              <>
+                <div>
+                  <img src={image.url} />
+                </div>
+
+              </>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
